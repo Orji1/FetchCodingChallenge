9d99822422f5ecb83ee1e3f06b66dcc7e27e25bd
Chimezie Orji-Unegbu <orjicv@gmail.com>
2025-01-04 22:59:53 -0500

    convert text to string resource

--- app/src/main/java/com/example/fetchapplication/presentation/composables/ErrorScreen.kt
+++ app/src/main/java/com/example/fetchapplication/presentation/composables/ErrorScreen.kt
@@ -6,10 +6,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.fetchapplication.R
 
 @Composable
 fun ErrorScreen() {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Oops Something Went Wrong")
+        Text(
+            stringResource(R.string.error_message),
+            fontSize = 40.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 44.sp
+        )
     }
 }
